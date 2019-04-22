d33986cfcf6c6f0cefdc5a5006f5be343c6888a7
FlorentinoJH <FlorentinoJH@outlook.com>
2019-04-22 16:11:49 +0800

    Revision: 34718

--- SequoiaDB/engine/pmd/pmdRemoteSession.cpp
+++ SequoiaDB/engine/pmd/pmdRemoteSession.cpp
@@ -1004,6 +1004,7 @@ namespace engine
                --totalUnReplyNum ;
             }
             _mapPendingSubSession.clear() ;
+            continue ;
          }
 
          if ( _pEDUCB->isInterrupted() )
@@ -1083,7 +1084,7 @@ namespace engine
       }
 
    done:
-      _totalWaitTime = _milliTimeoutHard - _milliTimeout ;
+      _totalWaitTime += ( _milliTimeoutHard - _milliTimeout ) ;
       return rc ;
    error:
       goto done ;
